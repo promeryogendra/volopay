63e9c24083d3c469993231cc8da45f614f107114
Yogendra R <yogendra@devathon.com>
2022-09-14 03:14:27 +0530

    Fix: store issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { connect } from "react-redux";
+import { setLoading } from "./store/actions/auth";
 import "./App.css";
 
-function App() {
-  return <div className="App">App</div>;
+function App(props) {
+  console.log(props.loading);
+  return (
+    <div onClick={(e) => props.setLoading(!props.loading)} className="App">
+      App
+    </div>
+  );
 }
+const mapStateToProps = (state) => {
+  return {
+    loading: state.auth.loading,
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, { setLoading })(App);
